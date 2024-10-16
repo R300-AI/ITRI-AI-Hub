c5d078a87f054632c817004613c2209a289e72cb
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-10-16 06:04:34 +0000

    Create setup_genio350.sh

---
+++ tools/setup_genio350.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip install numpy
+
+# NeuronPilot
+sudo apt install mediatek-vpud-genio350
+sudo apt install mediatek-mdpd-genio350
+
+# Gstreamer
+sudo apt install pulseaudio
+sudo apt install pulseaudio-utils
+sudo apt install gstreamer1.0-alsa
+sudo apt install gstreamer1.0-plugins-base
+sudo apt install gstreamer1.0-plugins-good
+sudo apt install gstreamer1.0-plugins-bad
+sudo rm ~/.cache/gstreamer-1.0/registry.aarch64.bin
+rm ~/.cache/gstreamer-1.0/registry.aarch64.bin
