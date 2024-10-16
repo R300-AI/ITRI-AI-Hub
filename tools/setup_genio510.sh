d107dfaad91124582ebef9d9d590696e2b21a2b4
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-10-16 06:03:53 +0000

    Create setup_genio510.sh

---
+++ tools/setup_genio510.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip install numpy
+
+# NeuronPilot
+sudo apt install mediatek-vpud-genio510
+sudo apt install mediatek-apusys-firmware-genio510
+sudo apt install mediatek-libneuron mediatek-neuron-utils mediatek-libneuron-dev
+
+# Gstreamer
+sudo apt install pulseaudio
+sudo apt install pulseaudio-utils
+sudo apt install gstreamer1.0-alsa
+sudo apt install gstreamer1.0-plugins-base
+sudo apt install gstreamer1.0-plugins-good
+sudo apt install gstreamer1.0-plugins-bad
+sudo rm ~/.cache/gstreamer-1.0/registry.aarch64.bin
+rm ~/.cache/gstreamer-1.0/registry.aarch64.bin
