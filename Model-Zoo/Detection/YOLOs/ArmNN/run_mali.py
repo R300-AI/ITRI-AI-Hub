8399ecd22fc137b41d79ed1006b969a707b044e4
MarkovChenITRI <140595764+MarkovChenITRI@users.noreply.github.com>
2024-11-04 16:55:44 +0800

    update

--- Model-Zoo/Detection/YOLOs/ArmNN/run_mali.py
+++ Model-Zoo/Detection/YOLOs/ArmNN/run_mali.py
@@ -1,6 +1,6 @@
 from utils import YOLOs, plot
 import cv2
-print(2)
+
 labels = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant',
           11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear',
           22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball',
