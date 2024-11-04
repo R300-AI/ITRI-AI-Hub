2aa9e1760920c07c78f4c4ff3d5b12f9ecf37cd9
MarkovChenITRI <140595764+MarkovChenITRI@users.noreply.github.com>
2024-11-04 16:42:54 +0800

    update

--- Model-Zoo/Detection/YOLOs/ArmNN/run_genio.py
+++ Model-Zoo/Detection/YOLOs/ArmNN/run_genio.py
@@ -1,6 +1,6 @@
 from utils import YOLOs, plot
 import cv2
-
+print(2)
 labels = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant',
           11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear',
           22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball',
@@ -10,7 +10,7 @@ labels = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5
           67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 
           78: 'hair drier', 79: 'toothbrush'}
 
-tflite_model = YOLOs(model_path='yolo8n_saved_model/yolo8n_float32.tflite')
+tflite_model = YOLOs(model_path='yolo8n_float32.tflite')
 results = tflite_model.predict([frame])
-
+print(results)
 cv2.imshow(plot(frame.copy(), results[0].copy(), labels))
