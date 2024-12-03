889f2741ad37aa6860f2e4d00cca86410118a272
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-12-04 00:15:16 +0800

    Update run.py

--- Model-Zoo/Semantic-Segmentation/SAMs(preview)/KleidiAI/run.py
+++ Model-Zoo/Semantic-Segmentation/SAMs(preview)/KleidiAI/run.py
@@ -7,17 +7,21 @@ parser.add_argument("-m", "--model_path", default='', type=str, help="Path to ON
 args = parser.parse_args()
 
 img = cv2.imread('./bus.jpg')
+
 # Run inference with bboxes prompt
 results = model(img, bboxes=[439, 437, 524, 709])
 print('Class:\n', [model.names[int(i.item())] for i in results[0].boxes.cls])
 print('Masks:\n', results[0].masks.xy)
+cv2.imshow(results[0].plot())
 
 # Run inference with points prompt
 results = model(img, points=[[200, 200]], labels=[1])
 print('Class:\n', [model.names[int(i.item())] for i in results[0].boxes.cls])
 print('Masks:\n', results[0].masks.xy)
+cv2.imshow(results[0].plot())
 
 # Run inference with texts prompt
 results = model(img, texts="a photo of a dog")
 print('Class:\n', [model.names[int(i.item())] for i in results[0].boxes.cls])
 print('Masks:\n', results[0].masks.xy)
+cv2.imshow(results[0].plot())
