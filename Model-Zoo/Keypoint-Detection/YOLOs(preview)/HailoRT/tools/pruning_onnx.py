468368681cc021bf03e686f6feb94e474a175b0c
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-12-09 17:56:03 +0800

    Update pruning_onnx.py

--- Model-Zoo/Keypoint-Detection/YOLOs(preview)/HailoRT/tools/pruning_onnx.py
+++ Model-Zoo/Keypoint-Detection/YOLOs(preview)/HailoRT/tools/pruning_onnx.py
@@ -4,32 +4,32 @@ from onnx import helper, TensorProto
 import onnx, onnxsim, yaml, argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--model_name", default='yolov8n-pose', type=str, help=".")
+parser.add_argument("-n", "--model_path", default='yolov8n-pose', type=str, help=".")
 args = parser.parse_args()
 
-model_name = args.model_name
+model_path = args.model_path
 
 with open('config.yaml', 'r') as f:
     config = yaml.load(f, Loader=yaml.SafeLoader)
-    end_nodes = config[model_name]
+    end_nodes = config[model_path]
 
     print('Pruning by following tensors:')
-    onnx_model = onnx.load(f'{model_name}.onnx')
+    onnx_model = onnx.load(f'{model_path}.onnx')
     intermediate_info = [tensor for tensor in onnx.shape_inference.infer_shapes(onnx_model).graph.value_info if tensor.name.split('_output_')[0] in end_nodes]
     for tensor in intermediate_info:
         print(' -', tensor.name)
 
 # Pruning onnx for Hailo
 if 'y' in input('Is that correct?').lower():
-    onnx_model = onnx.load(f'{model_name}.onnx')
+    onnx_model = onnx.load(f'{model_path}.onnx')
     onnx_model.graph.output.clear()
     onnx_model.graph.output.extend(intermediate_info)
     onnx.checker.check_model(onnx_model)
-    onnx.save(onnx_model, f'./{model_name}_front.onnx')
-    print(f'Front Section of ONNX saved to ./{model_name}_front.onnx')
+    onnx.save(onnx_model, f'{model_path}_front.onnx')
+    print(f'Front Section of ONNX saved to {model_path}_front.onnx')
 
     # add new input nodes
-    onnx_model = onnx.load(f'{model_name}.onnx')
+    onnx_model = onnx.load(f'{model_path}.onnx')
     onnx_model.graph.input.clear()
     new_inputs = []
     for input_name in end_nodes:
@@ -52,7 +52,7 @@ if 'y' in input('Is that correct?').lower():
 
     # remove unused nodes
     length = 0
-    invalid_nodes = [i.name for i in ort.InferenceSession(f'{model_name}.onnx').get_inputs()]
+    invalid_nodes = [i.name for i in ort.InferenceSession(f'{model_path}.onnx').get_inputs()]
     while len(invalid_nodes) != length:
         length = len(invalid_nodes)
         for destination in onnx_model.graph.node:
@@ -68,13 +68,13 @@ if 'y' in input('Is that correct?').lower():
 
     # save pruned model
     onnx_model, check = onnxsim.simplify(onnx_model)
-    onnx.save(onnx_model, f'{model_name}_end.onnx')
-    print(f'Rear Section of saved to ./{model_name}_end.onnx')
+    onnx.save(onnx_model, f'{model_path}_end.onnx')
+    print(f'Rear Section of saved to {model_path}_end.onnx')
 
 
 # Compare Origin Outpus and Concated Outputs of Pruned Model
 print('Consistency Check...')
-session = ort.InferenceSession(f'{model_name}.onnx')
+session = ort.InferenceSession(f'{model_path}.onnx')
 input_details =  [i for i in session.get_inputs()]
 output_details = [i.name for i in session.get_outputs()]
 outputs = session
@@ -82,7 +82,7 @@ outputs = session
 inputs = {i.name: np.zeros(i.shape).astype(np.float32) for i in input_details}
 origin_results = session.run(output_details, inputs)
 
-session_part1 = ort.InferenceSession(f'{model_name}_front.onnx')
+session_part1 = ort.InferenceSession(f'{model_path}_front.onnx')
 part1_input_details =  [i for i in session_part1.get_inputs()]
 part1_output_details = [i.name for i in session_part1.get_outputs()]
 assert [i.name for i in input_details] == [i.name for i in part1_input_details]
@@ -93,7 +93,7 @@ for i in session_part1.get_outputs():
 inputs = {i.name: np.zeros(i.shape).astype(np.float32) for i in part1_input_details}
 part1_results = session_part1.run(part1_output_details, inputs)
 
-session_part2 = ort.InferenceSession(f'{model_name}_end.onnx')
+session_part2 = ort.InferenceSession(f'{model_path}_end.onnx')
 part2_input_details =  [i for i in session_part2.get_inputs()]
 part2_output_details = [i.name for i in session_part2.get_outputs()]
 
